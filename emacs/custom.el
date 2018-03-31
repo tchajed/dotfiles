7cbe2bb42a8de1303a9675c2d768a9e3a7c02791
Tej Chajed <tchajed@mit.edu>
2018-03-31 09:03:03 -0400

    Update emacs config

--- emacs/custom.el
+++ emacs/custom.el
@@ -3,7 +3,13 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(coq-compiler "/Users/tchajed/code/sw/coq/bin/coqc")
+ '(coq-dependency-analyzer "/Users/tchajed/code/sw/coq/bin/coqc")
+ '(coq-prefer-top-of-conclusion t)
+ '(coq-prog-name "/Users/tchajed/code/sw/coq/bin/coqtop")
  '(evil-want-abbrev-expand-on-insert-exit nil)
+ '(flycheck-rust-cargo-executable "/Users/tchajed/.cargo/bin/cargo")
+ '(flycheck-rust-executable "/Users/tchajed/.cargo/bin/rustc")
  '(font-latex-match-type-command-keywords (quote (("todo" "[{"))))
  '(font-latex-user-keyword-classes
    (quote
@@ -14,12 +20,28 @@
  '(gofmt-command "goimports")
  '(package-selected-packages
    (quote
-    (rvm ruby-tools ruby-test-mode rubocop rspec-mode robe rbenv rake minitest chruby bundler inf-ruby go-guru browse-at-remote async s company-quickhelp diminish f fsharp-mode fstar-mode company-go proof-general go-mode projectile packed smartparens evil helm helm-core avy dash imenu-list coq-mode yapfify yaml-mode web-mode web-beautify utop tuareg caml toml-mode tide typescript-mode tagedit smeargle slim-mode scss-mode sass-mode racer pyvenv pytest pyenv-mode py-isort pug-mode pip-requirements orgit org-projectile org-present org-pomodoro alert log4e gntp org-download ocp-indent mmm-mode merlin markdown-toc markdown-mode magit-gitflow lua-mode livid-mode skewer-mode simple-httpd live-py-mode less-css-mode json-mode json-snatcher json-reformat js2-refactor multiple-cursors js2-mode js-doc intero hy-mode htmlize hlint-refactor hindent helm-pydoc helm-hoogle helm-gitignore helm-css-scss helm-company helm-c-yasnippet haskell-snippets haml-mode gnuplot gitignore-mode gitconfig-mode gitattributes-mode git-timemachine git-messenger git-link git-gutter-fringe+ git-gutter-fringe fringe-helper git-gutter+ git-gutter gh-md fuzzy flycheck-rust seq flycheck-pos-tip pos-tip flycheck-haskell flycheck evil-magit magit magit-popup git-commit with-editor emmet-mode diff-hl cython-mode company-web web-completion-data company-tern dash-functional tern company-statistics company-ghci company-ghc ghc haskell-mode company-coq company-math math-symbol-lists company-cabal company-auctex company-anaconda company coffee-mode cmm-mode cargo rust-mode auto-yasnippet yasnippet auctex anaconda-mode pythonic ac-ispell auto-complete ws-butler winum which-key volatile-highlights vi-tilde-fringe uuidgen use-package toc-org spaceline restart-emacs request rainbow-delimiters popwin persp-mode pcre2el paradox org-plus-contrib org-bullets open-junk-file neotree move-text macrostep lorem-ipsum linum-relative link-hint info+ indent-guide hungry-delete hl-todo highlight-parentheses highlight-numbers highlight-indentation hide-comnt help-fns+ helm-themes helm-swoop helm-purpose helm-projectile helm-mode-manager helm-make helm-flx helm-descbinds helm-ag google-translate golden-ratio flx-ido fill-column-indicator fancy-battery eyebrowse expand-region exec-path-from-shell evil-visualstar evil-visual-mark-mode evil-unimpaired evil-tutor evil-surround evil-search-highlight-persist evil-numbers evil-nerd-commenter evil-mc evil-matchit evil-lisp-state evil-indent-plus evil-iedit-state evil-exchange evil-escape evil-ediff evil-args evil-anzu eval-sexp-fu elisp-slime-nav dumb-jump define-word column-enforce-mode clean-aindent-mode auto-highlight-symbol auto-compile aggressive-indent adaptive-wrap ace-window ace-link ace-jump-helm-line)))
+    (proof-general fstar-mode adaptive-wrap auctex yasnippet-snippets yapfify yaml-mode ws-butler winum which-key web-mode web-beautify volatile-highlights vi-tilde-fringe uuidgen utop use-package tuareg toml-mode toc-org tide tagedit symon string-inflection spaceline-all-the-icons smeargle slim-mode scss-mode sass-mode rvm ruby-tools ruby-test-mode ruby-refactor ruby-hash-syntax rubocop rspec-mode robe restart-emacs rbenv rake rainbow-delimiters racer quick-peek pyvenv pytest pyenv-mode py-isort pug-mode psci psc-ide popwin pippel pipenv pip-requirements persp-mode password-generator paradox overseer orgit org-projectile org-present org-pomodoro org-mime org-download org-bullets org-brain open-junk-file ocp-indent neotree nameless move-text mmm-mode minitest merlin markdown-toc magit-gitflow macrostep lorem-ipsum livid-mode live-py-mode linum-relative link-hint less-css-mode json-mode js2-refactor js-doc intero indent-guide importmagic impatient-mode idris-mode hy-mode hungry-delete hlint-refactor hl-todo hindent highlight-parentheses highlight-numbers highlight-indentation helm-xref helm-themes helm-swoop helm-pydoc helm-purpose helm-projectile helm-mode-manager helm-make helm-hoogle helm-gitignore helm-flx helm-descbinds helm-css-scss helm-company helm-c-yasnippet helm-ag haskell-snippets google-translate golden-ratio godoctor go-tag go-rename go-guru go-eldoc gnuplot gitconfig-mode gitattributes-mode git-timemachine git-messenger git-link git-gutter-fringe git-gutter-fringe+ gh-md fuzzy fsharp-mode font-lock+ flycheck-rust flycheck-pos-tip flycheck-haskell flx-ido fill-column-indicator fancy-battery eyebrowse expand-region exec-path-from-shell evil-visualstar evil-visual-mark-mode evil-unimpaired evil-tutor evil-surround evil-search-highlight-persist evil-org evil-numbers evil-nerd-commenter evil-mc evil-matchit evil-magit evil-lisp-state evil-lion evil-indent-plus evil-iedit-state evil-exchange evil-escape evil-ediff evil-cleverparens evil-args evil-anzu eval-sexp-fu emmet-mode elisp-slime-nav editorconfig dumb-jump diminish diff-hl define-word dante cython-mode counsel-projectile company-web company-tern company-statistics company-lua company-go company-ghci company-ghc company-coq company-cabal company-auctex company-anaconda column-enforce-mode coffee-mode cmm-mode clean-aindent-mode chruby centered-cursor-mode cargo bundler browse-at-remote auto-yasnippet auto-highlight-symbol auto-compile auctex-latexmk aggressive-indent ace-window ace-link ace-jump-helm-line ac-ispell)))
  '(proof-shrink-windows-tofit t)
  '(racer-rust-src-path nil)
  '(safe-local-variable-values
    (quote
-    ((eval progn
+    ((buffer-file-coding-system . utf-8-unix)
+     (coq-prog-args "-noinit")
+     (eval let
+           ((default-directory
+              (locate-dominating-file buffer-file-name ".dir-locals.el")))
+           (setq-local coq-prog-args
+                       (\`
+                        ("-coqlib"
+                         (\,
+                          (expand-file-name ".."))
+                         "-R"
+                         (\,
+                          (expand-file-name "."))
+                         "Coq")))
+           (setq-local coq-prog-name
+                       (expand-file-name "../bin/coqtop")))
+     (eval progn
            (let
                ((plzoo-root-directory
                  (when buffer-file-name
@@ -102,6 +124,7 @@
                (setq default-directory coq-root-directory))))
      (reftex-default-bibliography "n.bib" "p.bib"))))
  '(search-invisible t)
+ '(sh-basic-offset 2)
  '(tab-width 2))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
