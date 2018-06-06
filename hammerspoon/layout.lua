1c55f145884140e72b352d3246c6cddfdf8b3a3e
Tej Chajed <tchajed@mit.edu>
2018-06-06 09:49:11 -0400

    Finish up setWorkLayout

--- hammerspoon/layout.lua
+++ hammerspoon/layout.lua
@@ -1,7 +1,10 @@
 workLayout = {
   {"com.spotify.client", nil, "Color LCD", hs.geometry.rect(0, 0, 0.66, 0.8), nil, nil},
   {"com.culturedcode.ThingsMac", nil, "Color LCD", hs.geometry.rect(0.66, 0, 0.33, 1), nil, nil},
+  {"com.google.Chrome", nil, "SyncMaster", hs.geometry.rect(0.1, 0, 0.8, 0.8), nil, nil},
 }
 function setWorkLayout()
   hs.layout.apply(workLayout)
 end
+
+hs.hotkey.bind({'shift', 'cmd'}, 'w', setWorkLayout)
