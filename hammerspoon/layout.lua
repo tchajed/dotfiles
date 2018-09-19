0df62220264c3474d7fde40497a4a57db6d403fc
Tej Chajed <tchajed@mit.edu>
2018-09-19 09:36:10 -0400

    Update work layout for BenQ

--- hammerspoon/layout.lua
+++ hammerspoon/layout.lua
@@ -1,7 +1,33 @@
+function gridUnitRec(rows, cols, x1, width, y1, height)
+  x1 = (x1-1)/cols
+  y1 = (y1-1)/rows
+  return hs.geometry.rect(x1, y1, width/cols, height/rows)
+end
+
+function layoutSpec(application, monitor, locationSpec)
+  if monitor == "mac" then
+    rows, cols = 3, 3
+    monitor = "Color LCD"
+  elseif monitor == "benq" then
+    rows, cols = 4, 6
+    monitor = "BenQ PD3200U"
+  end
+  return {application, nil, monitor,
+          gridUnitRec(rows, cols,
+                      locationSpec[1],
+                      locationSpec[2],
+                      locationSpec[3],
+                      locationSpec[4]),
+          nil, nil}
+end
+
 workLayout = {
-  {"com.spotify.client", nil, "Color LCD", hs.geometry.rect(0, 0, 0.66, 0.8), nil, nil},
-  {"com.culturedcode.ThingsMac", nil, "Color LCD", hs.geometry.rect(0.66, 0, 0.33, 1), nil, nil},
-  {"com.google.Chrome", nil, "SyncMaster", hs.geometry.rect(0.1, 0, 0.8, 0.8), nil, nil},
+  layoutSpec("com.spotify.client", "mac", {2, 2, 1, 3}),
+  layoutSpec("com.culturedcode.ThingsMac", "mac", {1, 1, 1, 2}),
+  layoutSpec("com.google.Chrome", "benq", {5, 2, 1, 2}),
+  layoutSpec("Emacs", "benq", {1, 2, 1, 4}),
+  layoutSpec("iTerm2", "benq", {5, 1, 3, 2}),
+  layoutSpec("WorkFlowy Beta", "benq", {6, 1, 3, 2}),
 }
 function setWorkLayout()
   hs.layout.apply(workLayout)
