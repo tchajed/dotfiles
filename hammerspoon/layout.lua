e463869857e9f1a5f9772c6344005a6b6c90e7e7
Tej Chajed <tchajed@mit.edu>
2018-10-23 09:48:12 -0400

    Open Spotify for work layout

--- hammerspoon/layout.lua
+++ hammerspoon/layout.lua
@@ -32,12 +32,29 @@ workLayout = {
   -- layoutSpec("com.culturedcode.ThingsMac", "benq", {6, 1, 3, 2}),
 
   layoutSpec("com.google.Chrome", "benq", {5, 2, 1, 2}),
-  layoutSpec("Emacs", "benq", {1, 2, 1, 4}),
+  layoutSpec("org.gnu.Emacs", "benq", {1, 2, 1, 4}),
   layoutSpec("Preview", "benq", {3, 2, 1, 4}),
   layoutSpec("iTerm2", "benq", {5, 1, 3, 2}),
 }
+
+function openSpotify()
+  app = hs.application.get("com.spotify.client")
+  if not app:focusedWindow() then
+    app:activate()
+  end
+end
+
+function focusEmacs()
+  app = hs.application.get("org.gnu.Emacs")
+  if app then
+    app:activate()
+  end
+end
+
 function setWorkLayout()
+  openSpotify()
   hs.layout.apply(workLayout)
+  focusEmacs()
 end
 
 hs.hotkey.bind({'shift', 'cmd'}, 'w', setWorkLayout)
