aad4bba9b6d07bcab4a9a47068eb092bf092ec40
Tej Chajed <tchajed@mit.edu>
2019-03-15 10:26:25 -0400

    Fix server-start and company-quickhelp-mode
    Finally don't have to do a bunch of work on Emacs startup

--- emacs/custom.el
+++ emacs/custom.el
@@ -5,6 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(TeX-auto-local ".auctex-auto")
  '(company-coq-disabled-features '(hello outline error-diffs))
+ '(company-quickhelp-mode nil)
  '(coq-compiler
    "/Users/tchajed/code/sw/coq/_build/install/default/bin/coqc")
  '(coq-dependency-analyzer
@@ -20,7 +21,7 @@
  '(font-latex-match-type-command-keywords '(("todo" "[{")))
  '(font-latex-user-keyword-classes '(("note" (("note" "[{")) font-lock-keyword-face command)))
  '(fstar-executable "/Users/tchajed/code/sw/everest/FStar/bin/fstar.exe")
- '(gofmt-command "goimports")
+ '(gofmt-command "goimports" t)
  '(js-indent-level 2)
  '(package-selected-packages
    '(racket-mode yasnippet-snippets yapfify yaml-mode ws-butler winum which-key web-mode web-beautify volatile-highlights vi-tilde-fringe uuidgen utop use-package tuareg toml-mode toc-org tide tagedit symon string-inflection spaceline-all-the-icons smeargle slim-mode seeing-is-believing scss-mode sass-mode rvm ruby-tools ruby-test-mode ruby-refactor ruby-hash-syntax rubocop rspec-mode robe restart-emacs rbenv rake rainbow-delimiters racer pyvenv pytest pyenv-mode py-isort pug-mode psci psc-ide proof-general prettier-js popwin pippel pipenv pip-requirements persp-mode password-generator paradox overseer orgit org-projectile org-present org-pomodoro org-mime org-download org-bullets org-brain open-junk-file ocp-indent neotree nameless move-text mmm-mode minitest markdown-toc magit-svn magit-gitflow macrostep lsp-ui lsp-rust lsp-python lsp-javascript-typescript lsp-go lorem-ipsum livid-mode live-py-mode link-hint json-navigator json-mode js2-refactor js-doc indent-guide importmagic impatient-mode idris-mode hungry-delete hlint-refactor hl-todo hindent highlight-parentheses highlight-numbers highlight-indentation helm-xref helm-themes helm-swoop helm-pydoc helm-purpose helm-projectile helm-org-rifle helm-mode-manager helm-make helm-hoogle helm-gitignore helm-git-grep helm-flx helm-descbinds helm-css-scss helm-company helm-c-yasnippet helm-ag haskell-snippets google-translate golden-ratio godoctor go-tag go-rename go-impl go-guru go-gen-test go-fill-struct go-eldoc gnuplot gitignore-templates gitconfig-mode gitattributes-mode git-timemachine git-messenger git-link git-gutter-fringe git-gutter-fringe+ gh-md fuzzy fstar-mode fsharp-mode font-lock+ flycheck-rust flycheck-pos-tip flycheck-ocaml flycheck-haskell flx-ido fill-column-indicator fancy-battery eyebrowse expand-region evil-visualstar evil-visual-mark-mode evil-unimpaired evil-tutor evil-surround evil-org evil-numbers evil-nerd-commenter evil-matchit evil-magit evil-lisp-state evil-lion evil-indent-plus evil-iedit-state evil-goggles evil-exchange evil-escape evil-ediff evil-cleverparens evil-args evil-anzu eval-sexp-fu emmet-mode elisp-slime-nav editorconfig dumb-jump dotenv-mode doom-modeline diminish diff-hl define-word cython-mode csv-mode counsel-projectile company-web company-tern company-statistics company-lua company-lsp company-go company-ghci company-coq company-cabal company-auctex company-anaconda column-enforce-mode cmm-mode clean-aindent-mode chruby centered-cursor-mode cargo bundler browse-at-remote auto-yasnippet auto-highlight-symbol auto-compile auctex-latexmk aggressive-indent ace-window ace-link ace-jump-helm-line ac-ispell))
