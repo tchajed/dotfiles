dfa134e694a5a6a4b4ddfb2ff6a7a42659ba7beb
Tej Chajed <tchajed@mit.edu>
2019-03-20 10:08:35 -0400

    Add a list of important Homebrew formulae

---
+++ homebrew-install.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+# Install a few essential Homebrew packages, especially if they're implicit
+# dependencies of scripts.
+
+pkgs="
+vitorgalvao/tiny-scripts/cask-repair
+cloc
+codemod
+coreutils
+curl
+diff-so-fancy
+fd
+fish
+fswatch
+fzf
+gcc
+git
+gnu-sed
+gnu-tar
+gnuplot
+go
+gpg-agent
+grep
+htop
+imagemagick
+make
+pandoc
+parallel
+reattach-to-user-namespace
+ripgrep
+rsync
+tmux
+tree
+wget
+youtube-dl
+"
+
+brew install $pkgs
