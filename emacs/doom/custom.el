76aa1586d6346c185744c697ac50acf835dc4822
Tej Chajed <tchajed@mit.edu>
2019-09-19 10:54:18 -0400

    Update Emacs custom config

--- emacs/doom/custom.el
+++ emacs/doom/custom.el
@@ -6,10 +6,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(safe-local-variable-values
-   (quote
-    ((intero-targets "replicated-disk:test:spec")
-     (intero-targets "replicated-disk:lib")))))
+ '(comment-multi-line t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
