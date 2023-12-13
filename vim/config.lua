a5d248e1884ee22dd730760024d3f6065cc7f4f1
Tej Chajed <tchajed@gmail.com>
2023-12-13 08:12:57 -0600

    Add more lvim configuration

--- vim/config.lua
+++ vim/config.lua
@@ -1,15 +1,61 @@
+-- vim: ts=2
 -- Read the docs: https://www.lunarvim.org/docs/configuration
 -- Video Tutorials: https://www.youtube.com/watch?v=sFA9kX-Ud_c&list=PLhoH5vyxr6QqGu0i7tt_XoVK9v-KvZ3m6
 -- Forum: https://www.reddit.com/r/lunarvim/
 -- Discord: https://discord.com/invite/Xb9B4Ny
 
 lvim.plugins = {
-  { "tjdevries/colorbuddy.vim" },
-  { "Th3Whit3Wolf/onebuddy" },
-  { "mlr-msft/vim-loves-dafny" },
-  { "preservim/vim-markdown" },
-  { "vim-latex/vim-latex" },
+	{ "tjdevries/colorbuddy.vim" },
+	{ "Th3Whit3Wolf/onebuddy" },
+	{ "preservim/vim-markdown" },
+	{ "vim-latex/vim-latex" },
+	{ "mlr-msft/vim-loves-dafny" },
+	{
+		"simrat39/symbols-outline.nvim",
+		config = function()
+			require("symbols-outline").setup()
+		end,
+	},
+	{
+		"jose-elias-alvarez/null-ls.nvim",
+		config = function()
+			local null_ls = require("null-ls")
+
+			null_ls.setup({
+				sources = {
+					null_ls.builtins.formatting.stylua,
+					null_ls.builtins.diagnostics.eslint,
+				},
+			})
+		end,
+	},
+	{
+		"nvim-telescope/telescope-project.nvim",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+		},
+		event = "BufWinEnter",
+	},
+	{
+		"ray-x/lsp_signature.nvim",
+		event = "BufRead",
+		config = function()
+			require("lsp_signature").on_attach()
+		end,
+	},
+	{
+		"folke/trouble.nvim",
+		cmd = "TroubleToggle",
+	},
+	{
+		"tzachar/cmp-tabnine",
+		build = "./install.sh",
+		dependencies = "hrsh7th/nvim-cmp",
+		event = "InsertEnter",
+	},
 }
+
+require("lspconfig").dafny.setup({})
 
 vim.cmd("set bg=light")
 
