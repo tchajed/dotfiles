ecc07134a5570f17981fd2eff77e5f7777203fcc
Tej Chajed <tchajed@gmail.com>
2024-01-09 15:45:29 -0600

    Auto-format some hammerspoon files

--- hammerspoon/layout.lua
+++ hammerspoon/layout.lua
@@ -1,96 +1,97 @@
 -- r should have rows, cols, x1, width, y1, height fields
 function gridUnitRec(r)
-  local x1 = (r.x1-1)/r.cols
-  local y1 = (r.y1-1)/r.rows
-  return hs.geometry.rect(x1, y1, r.width/r.cols, r.height/r.rows)
+	local x1 = (r.x1 - 1) / r.cols
+	local y1 = (r.y1 - 1) / r.rows
+	return hs.geometry.rect(x1, y1, r.width / r.cols, r.height / r.rows)
 end
 
-benQKeys = {"123456", "qwerty", "asdfgh", "zxcvbn"}
-dellwideKeys = {"12345678", "qwertyui", "asdfghjk", "zxcvbnm,"}
-macKeys = {"qwe", "asd", "zxc"}
+benQKeys = { "123456", "qwerty", "asdfgh", "zxcvbn" }
+dellwideKeys = { "12345678", "qwertyui", "asdfghjk", "zxcvbnm," }
+macKeys = { "qwe", "asd", "zxc" }
 
 -- translate a single location in a row mapping to 1-based (x,y) coordinations
 function cornerToCoords(keyRows, corner)
-  for i, row in pairs(keyRows) do
-    local idx = row:find(corner)
-    if idx then
-      return {x=idx, y=i}
-    end
-  end
+	for i, row in pairs(keyRows) do
+		local idx = row:find(corner)
+		if idx then
+			return { x = idx, y = i }
+		end
+	end
 end
 
 -- translate a corner specification (eg, "1x") using a key mapping (eg, benQKeys
 -- above) to a gridUnitRec rectangle specification
 function translateCornerSpec(keyRows, corners)
-  local rows = #keyRows
-  local cols = #keyRows[1]
-  local upperLeft = cornerToCoords(keyRows, corners:sub(1,1))
-  local lowerRight = cornerToCoords(keyRows, corners:sub(2,2))
-  return {
-    rows=#keyRows,
-    cols=#keyRows[1],
-    x1=upperLeft.x, width=lowerRight.x - upperLeft.x + 1,
-    y1=upperLeft.y, height=lowerRight.y - upperLeft.y + 1
-  }
+	local rows = #keyRows
+	local cols = #keyRows[1]
+	local upperLeft = cornerToCoords(keyRows, corners:sub(1, 1))
+	local lowerRight = cornerToCoords(keyRows, corners:sub(2, 2))
+	return {
+		rows = #keyRows,
+		cols = #keyRows[1],
+		x1 = upperLeft.x,
+		width = lowerRight.x - upperLeft.x + 1,
+		y1 = upperLeft.y,
+		height = lowerRight.y - upperLeft.y + 1,
+	}
 end
 
 function layoutSpec(application, monitor, corners)
-  local keyRows = nil
-  if monitor == "mac" then
-    keyRows = macKeys
-    monitor = "Color LCD"
-  elseif monitor == "benq" then
-    keyRows = benQKeys
-    monitor = "BenQ PD3200U"
-  elseif monitor == "dellwide" then
-    keyRows = dellwideKeys
-    monitor = "Dell U3818DW"
-  end
-  local rec = gridUnitRec(translateCornerSpec(keyRows, corners))
-  return {application, nil, monitor, rec,
-          nil, nil}
+	local keyRows = nil
+	if monitor == "mac" then
+		keyRows = macKeys
+		monitor = "Color LCD"
+	elseif monitor == "benq" then
+		keyRows = benQKeys
+		monitor = "BenQ PD3200U"
+	elseif monitor == "dellwide" then
+		keyRows = dellwideKeys
+		monitor = "Dell U3818DW"
+	end
+	local rec = gridUnitRec(translateCornerSpec(keyRows, corners))
+	return { application, nil, monitor, rec, nil, nil }
 end
 
 workLayout = {
-  layoutSpec("com.spotify.client", "mac", "wc"),
-  --layoutSpec("WorkFlowy Beta", "benq", "hn"),
-  layoutSpec("com.culturedcode.ThingsMac", "mac", "qa"),
-  layoutSpec("Firefox", "mac", "wc"),
+	layoutSpec("com.spotify.client", "mac", "wc"),
+	--layoutSpec("WorkFlowy Beta", "benq", "hn"),
+	layoutSpec("com.culturedcode.ThingsMac", "mac", "qa"),
+	layoutSpec("Firefox", "mac", "wc"),
 
-  -- temporary config for working on presentation
-  -- layoutSpec("com.spotify.client", "mac", "qx"),
-  -- layoutSpec("WorkFlowy Beta", "mac", "ec"),
-  -- layoutSpec("com.culturedcode.ThingsMac", "benq", "hn"),
+	-- temporary config for working on presentation
+	-- layoutSpec("com.spotify.client", "mac", "qx"),
+	-- layoutSpec("WorkFlowy Beta", "mac", "ec"),
+	-- layoutSpec("com.culturedcode.ThingsMac", "benq", "hn"),
 
-  -- layoutSpec("org.gnu.Emacs", "benq", "1v"),
-  -- layoutSpec("GoLand", "benq", "1v"),
-  -- layoutSpec("PyCharm", "benq", "1v"),
-  layoutSpec("org.gnu.Emacs", "dellwide", "2b"),
-  layoutSpec("GoLand", "dellwide", "2b"),
+	-- layoutSpec("org.gnu.Emacs", "benq", "1v"),
+	-- layoutSpec("GoLand", "benq", "1v"),
+	-- layoutSpec("PyCharm", "benq", "1v"),
+	layoutSpec("org.gnu.Emacs", "dellwide", "2b"),
+	layoutSpec("GoLand", "dellwide", "2b"),
 
-  -- layoutSpec("com.apple.Preview", "benq", "2v"),
-  -- layoutSpec("net.sourceforge.skim-app.skim", "benq", "3v"),
-  -- layoutSpec("iTerm2", "benq", "5n"),
-  layoutSpec("iTerm2", "dellwide", "6k"),
+	-- layoutSpec("com.apple.Preview", "benq", "2v"),
+	-- layoutSpec("net.sourceforge.skim-app.skim", "benq", "3v"),
+	-- layoutSpec("iTerm2", "benq", "5n"),
+	layoutSpec("iTerm2", "dellwide", "6k"),
 }
 
 function openSpotify()
-  -- ensures Spotify is open and has a window open
-  hs.application.open("com.spotify.client", 2, true)
+	-- ensures Spotify is open and has a window open
+	hs.application.open("com.spotify.client", 2, true)
 end
 
 function focusApp(name)
-  app = hs.application.get(name)
-  if app then
-    app:activate()
-  end
+	app = hs.application.get(name)
+	if app then
+		app:activate()
+	end
 end
 
 function setWorkLayout()
-  openSpotify()
-  hs.layout.apply(workLayout)
-  focusApp("Firefox")
-  focusApp("org.gnu.Emacs")
+	openSpotify()
+	hs.layout.apply(workLayout)
+	focusApp("Firefox")
+	focusApp("org.gnu.Emacs")
 end
 
-hs.hotkey.bind({'shift', 'cmd'}, 'w', setWorkLayout)
+-- hs.hotkey.bind({'shift', 'cmd'}, 'w', setWorkLayout)
--- hammerspoon/window.lua
+++ hammerspoon/window.lua
@@ -1,57 +1,84 @@
 -- Window hints (quick window selection)
 
 function reversed(l)
-  local reversedList = {}
-  for i=1,#l do
-    reversedList[i] = l[#l+1 - i]
-  end
-  return reversedList
+	local reversedList = {}
+	for i = 1, #l do
+		reversedList[i] = l[#l + 1 - i]
+	end
+	return reversedList
 end
 
 function updateTitleThresh()
-  local numScreens = #hs.screen.allScreens()
-  if numScreens > 1 then
-    -- above this many windows, titles are shown
-    hs.hints.showTitleThresh = 20
-  else
-    hs.hints.showTitleThresh = 5
-  end
+	local numScreens = #hs.screen.allScreens()
+	if numScreens > 1 then
+		-- above this many windows, titles are shown
+		hs.hints.showTitleThresh = 20
+	else
+		hs.hints.showTitleThresh = 5
+	end
 end
 updateTitleThresh()
-hs.hints.hintChars = {"A","S","D","F","G","H","J","K","L","Q","W","E","R","T","Y","U","I","O","P","C","V","B","N"}
+hs.hints.hintChars = {
+	"A",
+	"S",
+	"D",
+	"F",
+	"G",
+	"H",
+	"J",
+	"K",
+	"L",
+	"Q",
+	"W",
+	"E",
+	"R",
+	"T",
+	"Y",
+	"U",
+	"I",
+	"O",
+	"P",
+	"C",
+	"V",
+	"B",
+	"N",
+}
 hs.hotkey.bind("cmd", "escape", function()
-                 windows = hs.window.allWindows()
-                 hs.hints.windowHints(reversed(windows))
+	windows = hs.window.allWindows()
+	hs.hints.windowHints(reversed(windows))
 end)
 
 -- Throwing windows between screens (monitors)
 hs.window.animationDuration = 0 -- default is 0.2
 -- workaround for window:moveOneScreenEast() not working
 function moveWindow(where)
-  if hs.window.focusedWindow() then
-    local w = hs.window.frontmostWindow()
-    local s = hs.screen.mainScreen()
-    if (where == "east") then s = s:toEast()
-    elseif (where == "west") then s = s:toWest()
-    elseif (where == "south") then s = s:toSouth()
-    elseif (where == "north") then s = s:toNorth()
-    end
-    w:moveToScreen(s)
-  end
+	if hs.window.focusedWindow() then
+		local w = hs.window.frontmostWindow()
+		local s = hs.screen.mainScreen()
+		if where == "east" then
+			s = s:toEast()
+		elseif where == "west" then
+			s = s:toWest()
+		elseif where == "south" then
+			s = s:toSouth()
+		elseif where == "north" then
+			s = s:toNorth()
+		end
+		w:moveToScreen(s)
+	end
 end
 hs.hotkey.bind("cmd-ctrl", "right", function()
-  moveWindow("east")
+	moveWindow("east")
 end)
 hs.hotkey.bind("cmd-ctrl", "left", function()
-  moveWindow("west")
+	moveWindow("west")
 end)
 hs.hotkey.bind("cmd-ctrl", "up", function()
-  moveWindow("north")
+	moveWindow("north")
 end)
 hs.hotkey.bind("cmd-ctrl", "down", function()
-  moveWindow("south")
+	moveWindow("south")
 end)
-
 
 -- Throwing windows between spaces
 --
@@ -59,54 +86,58 @@ end)
 -- on the current screen.
 
 function indexOf(needle, a)
-  for i=1,#a do
-    if a[i] == needle then
-      return i
-    end
-  end
-  return nil
+	for i = 1, #a do
+		if a[i] == needle then
+			return i
+		end
+	end
+	return nil
 end
 
 function moveWindowToSpace(where)
-  local win = hs.window.focusedWindow()
-  local space = hs.spaces.focusedSpace()
-  -- local spaces = hs.spaces.windowSpaces(win)
-  -- if #spaces ~= 1 then
-  --   -- unusual window that is on multiple spaces
-  --   return nil
-  -- end
-  -- local space = spaces[1]
+	local win = hs.window.focusedWindow()
+	local space = hs.spaces.focusedSpace()
+	-- local spaces = hs.spaces.windowSpaces(win)
+	-- if #spaces ~= 1 then
+	--   -- unusual window that is on multiple spaces
+	--   return nil
+	-- end
+	-- local space = spaces[1]
 
-  local screen = hs.screen.mainScreen()
-  local spaces = hs.spaces.spacesForScreen(screen)
-  local currentIndex = indexOf(space, spaces)
-  if currentIndex == nil then
-    -- this is an error, something is unusual about focused space/screen
-    return nil
-  end
-  local destIndex = currentIndex + where
-  -- if destination is out-of-bounds, then do nothing
-  if not (1 <= destIndex and destIndex <= #spaces) then
-    return nil
-  end
-  local destSpace = spaces[destIndex]
-  hs.spaces.moveWindowToSpace(win, destSpace)
-  -- NOTE: focusing the window will switch spaces, but unlike
-  -- hs.spaces.gotoSpace() does not open up mission control
-  win:focus()
+	local screen = hs.screen.mainScreen()
+	local spaces = hs.spaces.spacesForScreen(screen)
+	local currentIndex = indexOf(space, spaces)
+	if currentIndex == nil then
+		-- this is an error, something is unusual about focused space/screen
+		return nil
+	end
+	local destIndex = currentIndex + where
+	-- if destination is out-of-bounds, then do nothing
+	if not (1 <= destIndex and destIndex <= #spaces) then
+		return nil
+	end
+	local destSpace = spaces[destIndex]
+	hs.spaces.moveWindowToSpace(win, destSpace)
+	-- NOTE: focusing the window will switch spaces, but unlike
+	-- hs.spaces.gotoSpace() does not open up mission control
+	win:focus()
 end
 
-hs.hotkey.bind({'ctrl', 'shift', 'cmd'}, 'left', function() moveWindowToSpace(-1) end)
-hs.hotkey.bind({'ctrl', 'shift', 'cmd'}, 'right', function() moveWindowToSpace(1) end)
+hs.hotkey.bind({ "ctrl", "shift", "cmd" }, "left", function()
+	moveWindowToSpace(-1)
+end)
+hs.hotkey.bind({ "ctrl", "shift", "cmd" }, "right", function()
+	moveWindowToSpace(1)
+end)
 
 -- Resizing windows
 -- adapted from Anish Athalye
 
 function moveTo(rect)
-  -- move the current window to th unit rect
-  -- a unit is an on-screen rectangle specified in relative coordinates:
-  --   {x,y,w,h} where all components are between 0 and 1
-  hs.window.focusedWindow():moveToUnit(rect)
+	-- move the current window to th unit rect
+	-- a unit is an on-screen rectangle specified in relative coordinates:
+	--   {x,y,w,h} where all components are between 0 and 1
+	hs.window.focusedWindow():moveToUnit(rect)
 end
 
 -- half of screen
@@ -114,48 +145,54 @@ end
 -- hs.hotkey.bind({'ctrl', 'cmd'}, 'l', function() moveTo(hs.layout.right50) end )
 
 -- quarter of screen
-hs.hotkey.bind({'shift', 'alt', 'cmd'}, 'left',
-  function() moveTo({0, 0, 0.5, 0.5}) end)
-hs.hotkey.bind({'shift', 'alt', 'cmd'}, 'right',
-  function() moveTo({0.5, 0.5, 0.5, 0.5}) end)
-hs.hotkey.bind({'shift', 'alt', 'cmd'}, 'up',
-  function() moveTo({0.5, 0, 0.5, 0.5}) end)
-hs.hotkey.bind({'shift', 'alt', 'cmd'}, 'down',
-  function() moveTo({0, 0.5, 0.5, 0.5}) end)
+hs.hotkey.bind({ "shift", "alt", "cmd" }, "left", function()
+	moveTo({ 0, 0, 0.5, 0.5 })
+end)
+hs.hotkey.bind({ "shift", "alt", "cmd" }, "right", function()
+	moveTo({ 0.5, 0.5, 0.5, 0.5 })
+end)
+hs.hotkey.bind({ "shift", "alt", "cmd" }, "up", function()
+	moveTo({ 0.5, 0, 0.5, 0.5 })
+end)
+hs.hotkey.bind({ "shift", "alt", "cmd" }, "down", function()
+	moveTo({ 0, 0.5, 0.5, 0.5 })
+end)
 
 -- full screen
-hs.hotkey.bind({'alt', 'cmd'}, 'f', function() moveTo(hs.layout.maximized) end)
+hs.hotkey.bind({ "alt", "cmd" }, "f", function()
+	moveTo(hs.layout.maximized)
+end)
 
 -- center screen
-hs.hotkey.bind({'alt', 'cmd'}, 'c', function()
-    hs.window.focusedWindow():centerOnScreen()
-    leftBorder = 0.25
-    topBorder = 0.18
-    moveTo({leftBorder, topBorder, (1-2*leftBorder), (1-2*topBorder)})
+hs.hotkey.bind({ "alt", "cmd" }, "c", function()
+	hs.window.focusedWindow():centerOnScreen()
+	leftBorder = 0.25
+	topBorder = 0.18
+	moveTo({ leftBorder, topBorder, (1 - 2 * leftBorder), (1 - 2 * topBorder) })
 end)
 
 -- grid gui
 --
 -- per-monitor configuration
 -- to identify monitors, look at hs.screen.allScreens()[1] (and possibly [2])
-hs.grid.setGrid('4x3', 'HP ZR2740w')
-hs.grid.setGrid('3x3', 'Color LCD')
-hs.grid.setGrid('8x4', 'BenQ PD3200U')
-hs.grid.setGrid('8x4', 'LG HDR 4K')
-hs.grid.setGrid('6x4', 'U2790B')
+hs.grid.setGrid("4x3", "HP ZR2740w")
+hs.grid.setGrid("3x3", "Color LCD")
+hs.grid.setGrid("8x4", "BenQ PD3200U")
+hs.grid.setGrid("8x4", "LG HDR 4K")
+hs.grid.setGrid("6x4", "U2790B")
 -- monitor has column of broken pixels on left side; avoid using that strip by
 -- starting at x=220 (y=25 is there in the default frame, see
 -- hs.screen.find("Dell U3818DW"):frame())
 -- hs.grid.setGrid('8x4', 'Dell U3818DW', hs.geometry(220, 25, 3200, 1308))
 
 -- now using different monitor that isn't broken
-hs.grid.setGrid('8x4', 'Dell U3818DW')
-hs.grid.setGrid('8x4', 'Dell C3422WE')
-hs.grid.setGrid('8x4', 'Dell U2723QE')
-hs.grid.setGrid('8x4', 'Dell U3223QE')
+hs.grid.setGrid("8x4", "Dell U3818DW")
+hs.grid.setGrid("8x4", "Dell C3422WE")
+hs.grid.setGrid("8x4", "Dell U2723QE")
+hs.grid.setGrid("8x4", "Dell U3223QE")
 
-hs.grid.setMargins({w = 0, h = 0})
-hs.hotkey.bind({'shift', 'cmd'}, 'g', function()
-  updateTitleThresh()
-  hs.grid.show()
+hs.grid.setMargins({ w = 0, h = 0 })
+hs.hotkey.bind({ "shift", "cmd" }, "g", function()
+	updateTitleThresh()
+	hs.grid.show()
 end)
