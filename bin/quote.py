0ac12b45521291d4f52d83e0806cb41b7108e548
Tej Chajed <tchajed@gmail.com>
2025-03-03 08:27:42 -0600

    black format

--- bin/quote.py
+++ bin/quote.py
@@ -1,14 +1,17 @@
 #!/usr/bin/env python3
 
+
 def add_quotes_if_necessary(s):
     if " " in s:
         return '"' + s + '"'
     return s
 
+
 def quote(path):
     components = path.split("/")
     components = [add_quotes_if_necessary(c) for c in components]
     return "/".join(components)
+
 
 import sys
 
