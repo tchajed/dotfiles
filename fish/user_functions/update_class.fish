3adfd15381b2d0a9bfd7d4afc0da0a68d43c574d
Tej Chajed <tchajed@gmail.com>
2025-10-13 16:02:12 -0500

    update class script

--- fish/user_functions/update_class.fish
+++ fish/user_functions/update_class.fish
@@ -1,8 +1,12 @@
 function update_class
-  pushd $HOME/code/cs839-fa24/sys-verif-solutions
-  ./etc/update-web.sh ../sys-verif-fa24
-  ./etc/template repo ../sys-verif-fa24-proofs
-  ./etc/update-goose.sh
-  make -k $(ls src/Goose/sys_verif_code/*.v | sed 's/\.v$/.vo/') 1>/dev/null
-  popd
+    pushd $HOME/code/cs839-fa25/sys-verif-solutions
+    echo "updating web"
+    ./etc/template web ../sys-verif-fa25
+    echo "updating repo"
+    ./etc/template repo ../sys-verif-fa25-proofs
+    echo "updating goose"
+    ./etc/update-goose.sh
+    make -j8 -k $(ls src/code/sys_verif_code/**.v | sed 's/\.v$/.vo/') 1>/dev/null
+    make -j8 -k $(ls src/generatedproof/sys_verif_code/**.v | sed 's/\.v$/.vo/') 1>/dev/null
+    popd
 end
